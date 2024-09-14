d13d465b3f75fd4f3b526fe1003f34e50f5cc02e
Zenith <zenithyap88@gmail.com>
2024-09-14 12:30:52 +0800

    Add footer test

---
+++ client/src/components/footer.test.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Footer from "./Footer";
+import { render } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import '@testing-library/jest-dom/extend-expect';
+
+describe('Footer component', () => {
+    test('renders footer component', () => {
+        const { getByText } = render(
+            <Router>
+                <Footer/>
+            </Router>
+        )
+        expect(getByText('All Rights Reserved © TestingComp')).toBeInTheDocument();
+        expect(getByText('About')).toBeInTheDocument();
+        expect(getByText('Contact')).toBeInTheDocument();
+        expect(getByText('Privacy Policy')).toBeInTheDocument();
+    });
+
+    test('renders all links with correct attribute', () => {
+        const { getByText } = render(
+            <Router>
+                <Footer/>
+            </Router>
+        )
+        expect(getByText('About')).toHaveAttribute('href', '/about');
+        expect(getByText('Contact')).toHaveAttribute('href', '/contact');
+        expect(getByText('Privacy Policy')).toHaveAttribute('href', '/policy');
+    })
+})  
