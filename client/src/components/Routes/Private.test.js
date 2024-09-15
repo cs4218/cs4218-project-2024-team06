0720f9a74aa5d54c308b3bdf50481964ae4b4df9
kohkaixun <kkx1929@gmail.com>
2024-09-16 02:21:28 +0800

    Create Private route test for failed Axios request Currently not working, need to fix test. mockRejectedValueOnce is not working as expected.

--- client/src/components/Routes/Private.test.js
+++ client/src/components/Routes/Private.test.js
@@ -3,9 +3,11 @@ import axios from 'axios';
 import { Outlet } from 'react-router-dom';
 import PrivateRoute from './Private';
 import React from 'react';
-import { findByTestId, findByText, render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import Spinner from "../Spinner";
 import { useAuth } from '../../context/auth';
+import { waitFor } from '@testing-library/react';
+import { clear } from 'console';
 
 jest.mock('../../context/auth', () => ({
     useAuth: jest.fn()
@@ -31,10 +33,14 @@ jest.mock('../Spinner', () =>
 );
 
 describe('PrivateRoute', () => {
-    it('should render Spinner if no auth token', () => {
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
+
+    it('should render Spinner if no auth token', async () => {
         useAuth.mockReturnValue([null, jest.fn()]);
         render(<PrivateRoute />);
-        expect(screen.getByText('Mocked Spinner')).toBeInTheDocument();
+        expect(await screen.findByText('Mocked Spinner')).toBeInTheDocument();
     });
     
     it('should render Outlet if axios reports valid token', async () => {
@@ -45,18 +51,18 @@ describe('PrivateRoute', () => {
         expect(await screen.findByText('Mocked Outlet')).toBeInTheDocument();
     });
 
-    it('should render Spinner if axios reports invalid token', () => {
+    it('should render Spinner if axios reports invalid token', async () => {
         useAuth.mockReturnValue([mockAuth, jest.fn()]);
         const res = { data: { ok: false } };
         axios.get.mockResolvedValue(res);
         render(<PrivateRoute />);
-        expect(screen.getByText('Mocked Spinner')).toBeInTheDocument();
+        expect(await screen.findByText('Mocked Spinner')).toBeInTheDocument();
     });
 
-    // it('should render Spinner if axios request fails', () => {
-    //     useAuth.mockReturnValue([mockAuth, jest.fn()]);
-    //     axios.get.mockRejectedValue(new Error('Async error'));
-    //     render(<PrivateRoute />);
-    //     expect(screen.getByText('Mocked Spinner')).toBeInTheDocument();
-    // });
+    it('should render Spinner if axios request fails', async () => {
+        useAuth.mockReturnValue([mockAuth, jest.fn()]);
+        axios.get.mockRejectedValue(new Error('arghhh'));
+        render(<PrivateRoute />);
+        expect(await screen.findByText('Mocked Spinner')).toBeInTheDocument();
+    });
 });
