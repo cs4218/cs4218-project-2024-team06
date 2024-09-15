1950d3a34728ae9f1c987be4b5a935bebde4a0de
eugenetangkj <dragoneugene10@gmail.com>
2024-09-15 13:27:41 +0800

    Add exception flow tests for registerController

--- controllers/authControllerRegisterController.test.js
+++ controllers/authControllerRegisterController.test.js
@@ -1,31 +1,46 @@
 import { registerController } from './authController';
+import userModel from '../models/userModel.js';
+import { describe } from 'node:test';
+
 
 // Mock functions
 jest.mock('./../helpers/authHelper', () => ({
-    // comparePassword:
-    hashPassword: jest.fn(async (password) => Promise.resolve(password)),
+    hashPassword: jest.fn(async (password) => Promise.resolve(`Mock ${password}`)),
 }));
+
+jest.mock('../models/userModel', () => {
+    //Need to mock the constructor because the constructor is used in registerController
+    const mockSave = jest.fn().mockResolvedValue({
+        name: "James",
+        email: "james",
+        password: "james123",
+        phone: "9123 4567",
+        address: "Sentosa",
+        answer: "Badminton"
+    });
+
+    return jest.fn().mockImplementation(() => ({
+        save: mockSave,
+    }));
+})
+
 
 // Test suite for registerController
 describe('registerController', () => {
+    let res;
     
-
     beforeEach(() => {
         jest.clearAllMocks();
+
+        //Mock the status and send functions
+        res = {
+            send: jest.fn(),
+            status: jest.fn().mockReturnThis()
+        };
     });
 
    
-    describe('should return error message if', () => {
-        let res;
-
-        beforeEach(() => {
-            //Mock the send function
-            res = {
-                send: jest.fn()
-            };
-        });
-
-
+    describe('should return error messages for its input validations if', () => {
         it('name is empty', async () => {
             //ARRANGE
             const req = {
@@ -301,28 +316,137 @@ describe('registerController', () => {
             expect(res.send).toHaveBeenCalledWith({ message: "Answer is Required" });
         });
 
-        // it('answer is purely whitespace', async () => {
-        //     //ARRANGE
-        //     const req = {
-        //         body: {
-        //             name: "James",
-        //             email: "james",
-        //             password: "james123",
-        //             phone: "9123 4567",
-        //             address: "",
-        //             answer: " "
-        //         }
-        //     };
-    
-        //     //ACTION
-        //     await registerController(req, res);
 
-            
-        //     //ASSERT
-        //     expect(res.send).toHaveBeenCalledTimes(1);
-        //     expect(res.send).toHaveBeenCalledWith({ message: "Answer is Required" });
-        // });
+        it('answer is purely whitespace', async () => {
+            //ARRANGE
+            const req = {
+                body: {
+                    name: "James",
+                    email: "james",
+                    password: "james123",
+                    phone: "9123 4567",
+                    address: "Sentosa",
+                    answer: " "
+                }
+            };
+            userModel.findOne = jest.fn().mockReturnValue({ name: "James" }); //As long as not null
+
+
+            //ACTION
+            await registerController(req, res);
+
+
+            //ASSERT
+            expect(res.send).toHaveBeenCalledTimes(1);
+            expect(res.send).toHaveBeenCalledWith({ message: "Answer is Required" });
+        });
+    });
+
+
+    describe('should correctly handle validating user against database where', () => {
+        let req;
+        beforeEach(() => {
+            req = {
+                body: {
+                    name: "James",
+                    email: "james",
+                    password: "james123",
+                    phone: "9123 4567",
+                    address: "Sentosa",
+                    answer: "Badminton"
+                }
+            };
+        });
+
+
+        it('it throws error if user can be found in database', async () => {
+            //ARRANGE
+            userModel.findOne = jest.fn().mockReturnValue({ name: "James" }); //As long as not null
+
+            //ACTION
+            await registerController(req, res);
+
+            //ASSERT
+            expect(res.status).toHaveBeenCalledTimes(1);
+            expect(res.status).toHaveBeenCalledWith(200);
+            expect(res.send).toHaveBeenCalledTimes(1);
+            expect(res.send).toHaveBeenCalledWith({ success: false, message: "Already Registered please login" });
+        });
+
+
+        it('it successfully returns if user cannot be found in database', async () => {
+            //ARRANGE
+            userModel.findOne = jest.fn().mockReturnValue(null);
+
+            //ACTION
+            await registerController(req, res);
+
+            //ASSERT
+            expect(res.status).toHaveBeenCalledTimes(1);
+            expect(res.status).toHaveBeenCalledWith(201);
+            expect(res.send).toHaveBeenCalledTimes(1);
+            expect(res.send).toHaveBeenCalledWith({ success: true, message: "User Register Successfully", user: {
+                name: "James",
+                email: "james",
+                password: "james123",
+                phone: "9123 4567",
+                address: "Sentosa",
+                answer: "Badminton"                
+            }});
+        });
+
+
+
+
 
 
     });
+
+
+
+    describe('should correctly handle exceptions by gracefully returning error message', () => {
+        let consoleLogSpy;
+        
+        beforeEach(() => {
+            //Reinitialise console log spy
+            consoleLogSpy = jest.spyOn(console, 'log').mockImplementation(() => {});
+        });
+
+
+        afterEach(() => {
+            //Restore original functionality of console.log
+            consoleLogSpy.mockRestore();
+        });
+
+
+        it('by gracefully returning error message', async () => {
+            //ARRANGE
+            const req = {
+                body: {
+                    name: "James",
+                    email: "james",
+                    password: "james123",
+                    phone: "9123 4567",
+                    address: "Sentosa",
+                    answer: "Badminton"
+                }
+            };
+            const error = new Error('Exception during registration');
+            userModel.findOne = jest.fn().mockRejectedValueOnce(error);
+
+            //ACTION
+            await registerController(req, res);
+
+            //ASSERT
+            expect(res.status).toHaveBeenCalledTimes(1);
+            expect(res.status).toHaveBeenCalledWith(500);
+            expect(res.send).toHaveBeenCalledTimes(1);
+            expect(res.send).toHaveBeenCalledWith({ success: false, message: "Error in Registration", error });
+        });
+    });
+
+
+
+
+
 });
