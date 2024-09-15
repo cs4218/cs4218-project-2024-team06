64d3b48d0d8b352f0d373939079c8e80ab033320
kohkaixun <kkx1929@gmail.com>
2024-09-15 17:29:05 +0800

    Create Private component test case for null auth

---
+++ client/src/components/Routes/Private.test.js
@@ -0,0 +1,41 @@
+import '@testing-library/jest-dom';
+import axios from 'axios';
+import { Outlet } from 'react-router-dom';
+import PrivateRoute from './Private';
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Spinner from "../Spinner";
+import { useAuth } from '../../context/auth';
+
+jest.mock('../../context/auth', () => ({
+    useAuth: jest.fn()
+}));
+
+jest.mock('axios');
+
+jest.mock('react-router-dom', () => ({
+    Outlet: () => <div>Mocked Outlet</div>
+}));
+
+jest.mock('../Spinner', () => 
+    () => <div>Mocked Spinner</div>
+);
+
+describe('PrivateRoute', () => {
+    it('should render Spinner if no auth token', () => {
+        useAuth.mockReturnValue([null, jest.fn()]);
+        render(<PrivateRoute />);
+        expect(screen.getByText('Mocked Spinner')).toBeInTheDocument();
+    });
+    
+    // it('should render Outlet if auth token is valid', () => {
+    //     useAuth.mockReturnValue([null, jest.fn()]);
+    //     const res = { data: { ok: true } };
+    //     axios.get.mockResolvedValue(res);
+    // });
+
+    // it('should render Spinner if auth token is invalid', () => {
+    //     const res = { data: { ok: false } };
+    //     axios.get.mockResolvedValue(res);
+    // });
+});
