207e7515b42aae8caf912753734631c8c8e56686
kohkaixun <kkx1929@gmail.com>
2024-09-15 17:35:07 +0800

    Add Private route test case for valid token

--- client/src/components/Routes/Private.test.js
+++ client/src/components/Routes/Private.test.js
@@ -11,6 +11,12 @@ jest.mock('../../context/auth', () => ({
     useAuth: jest.fn()
 }));
 
+jest.mock('mongoose', () => ({
+    connect: jest.fn(),
+    model: jest.fn(),
+    Schema: jest.fn(),
+}));
+
 jest.mock('axios');
 
 jest.mock('react-router-dom', () => ({
@@ -28,13 +34,14 @@ describe('PrivateRoute', () => {
         expect(screen.getByText('Mocked Spinner')).toBeInTheDocument();
     });
     
-    // it('should render Outlet if auth token is valid', () => {
-    //     useAuth.mockReturnValue([null, jest.fn()]);
-    //     const res = { data: { ok: true } };
-    //     axios.get.mockResolvedValue(res);
-    // });
+    it('should render Outlet if auth token is valid', () => {
+        useAuth.mockReturnValue([{token: true}, jest.fn()]);
+        const res = { data: { ok: true } };
+        axios.get.mockResolvedValue(res);
+    });
 
     // it('should render Spinner if auth token is invalid', () => {
+    //     useAuth.mockReturnValue([{token: false}, jest.fn()]);
     //     const res = { data: { ok: false } };
     //     axios.get.mockResolvedValue(res);
     // });
