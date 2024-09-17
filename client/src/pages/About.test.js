98e502746542366a331480c6faecd96ff1da9958
Zenith <zenithyap88@gmail.com>
2024-09-17 14:34:02 +0800

    Add test for About component

---
+++ client/src/pages/About.test.js
@@ -0,0 +1,40 @@
+import React from "react";
+import About from "./About";
+import Layout from "../components/Layout";
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import { beforeEach } from "node:test";
+
+// Mock layout component
+jest.mock('../components/Layout', () => ({title, children}) => (
+    <div data-testid='mock-layout'>
+        <h1>{title}</h1>
+        <div>{children}</div>
+    </div>
+));
+
+describe('About component', () => {
+    beforeEach(() => {
+        jest.clearAllMocks();
+    })
+
+    test('renders text on page correctly', () => {
+        render(<About/>);
+
+        // Assert that layout component is rendered
+        expect(screen.getByTestId('mock-layout')).toBeInTheDocument();
+
+        // Assert text rendering
+        expect(screen.getByText('About us - Ecommerce app')).toBeInTheDocument();
+        expect(screen.getByText('Add text')).toBeInTheDocument();
+    })
+
+    //NEVER PASS
+    test('renders images on page correctly', () => {
+        render(<About/>);
+        const image = screen.getByAltText("aboutus");
+
+        expect(image).toBeInTheDocument();
+        expect(image).toHaveAttribute('src', '/images/about.jpeg');
+    })
+});
