ad7d034c15e0e4d7712b0f4f9ef174908f66f17e
eugenetangkj <dragoneugene10@gmail.com>
2024-09-17 23:00:48 +0800

    Add checking of input fields in Register component

--- client/src/pages/Auth/Register.test.js
+++ client/src/pages/Auth/Register.test.js
@@ -91,6 +91,75 @@ describe('Register Component', () => {
         //   expect(screen.getByPlaceholderText('Enter Your DOB')).toHaveAttribute('type', 'date');
         //   expect(screen.getByPlaceholderText('What Is Your Favorite Sport')).toHaveAttribute('type', 'text');
         // });
+
+        it('where the input fields should all initially be empty', () => {
+          //ARRANGE
+  
+          //ACTION
+          render(
+            <MemoryRouter initialEntries={['/register']}>
+              <Routes>
+                <Route path="/register" element={<Register />} />
+              </Routes>
+            </MemoryRouter>
+          );
+  
+          //ASSERT
+          expect(screen.getByPlaceholderText('Enter Your Name').value).toBe('');
+          expect(screen.getByPlaceholderText('Enter Your Email').value).toBe('');
+          expect(screen.getByPlaceholderText('Enter Your Password').value).toBe('');
+          expect(screen.getByPlaceholderText('Enter Your Phone').value).toBe('');
+          expect(screen.getByPlaceholderText('Enter Your Address').value).toBe('');
+          expect(screen.getByPlaceholderText('Enter Your DOB').value).toBe('');
+          /* Previous test already caught error with the placeholder text for the sports field
+             Hence, I still use the wrong placeholder because intention of this test is to check initial value
+             and not label
+          */
+          expect(screen.getByPlaceholderText('What is Your Favorite sports').value).toBe('');
+        });
+
+
+        it('where the input fields should allow typing', () => {
+          //ARRANGE
+          const sampleInput = {
+            name: 'James',
+            email: 'james@gmail.com',
+            password: 'password',
+            phone: '91234567',
+            address: 'Sentosa',
+            dob: '2020-05-05',
+            sport: 'Badminton'
+          };
+
+          //ACTION
+          render(
+            <MemoryRouter initialEntries={['/register']}>
+              <Routes>
+                <Route path="/register" element={<Register />} />
+              </Routes>
+            </MemoryRouter>
+          );
+          fireEvent.change(screen.getByPlaceholderText('Enter Your Name'), { target: { value: sampleInput.name } });
+          fireEvent.change(screen.getByPlaceholderText('Enter Your Email'), { target: { value: sampleInput.email } });
+          fireEvent.change(screen.getByPlaceholderText('Enter Your Password'), { target: { value: sampleInput.password } });
+          fireEvent.change(screen.getByPlaceholderText('Enter Your Phone'), { target: { value: sampleInput.phone } });
+          fireEvent.change(screen.getByPlaceholderText('Enter Your Address'), { target: { value: sampleInput.address } });
+          fireEvent.change(screen.getByPlaceholderText('Enter Your DOB'), { target: { value: sampleInput.dob } });
+          fireEvent.change(screen.getByPlaceholderText('What is Your Favorite sports'), { target: { value: sampleInput.sport } });
+
+          //ASSERT
+          expect(screen.getByPlaceholderText('Enter Your Name').value).toBe(sampleInput.name);
+          expect(screen.getByPlaceholderText('Enter Your Email').value).toBe(sampleInput.email);
+          expect(screen.getByPlaceholderText('Enter Your Password').value).toBe(sampleInput.password);
+          expect(screen.getByPlaceholderText('Enter Your Phone').value).toBe(sampleInput.phone);
+          expect(screen.getByPlaceholderText('Enter Your Address').value).toBe(sampleInput.address);
+          expect(screen.getByPlaceholderText('Enter Your DOB').value).toBe(sampleInput.dob);
+          /* Previous test already caught error with the placeholder text for the sports field
+             Hence, I still use the wrong placeholder because intention of this test is to check whether the
+             input fields allow typing and not label
+          */
+          expect(screen.getByPlaceholderText('What is Your Favorite sports').value).toBe(sampleInput.sport);
+        });
       });
 
 
