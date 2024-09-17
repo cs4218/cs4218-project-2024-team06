83e7ab8a100c1a24bd000c39f1ec2eab0c24ba6a
eugenetangkj <dragoneugene10@gmail.com>
2024-09-18 00:28:13 +0800

    Complete tests for register functionality

--- client/src/pages/Auth/Register.test.js
+++ client/src/pages/Auth/Register.test.js
@@ -6,14 +6,52 @@ import '@testing-library/jest-dom/extend-expect';
 import toast from 'react-hot-toast';
 import Register from './Register';
 
+//CONSTANTS AND HELPER FUNCTIONS
+const sampleInput = {
+  name: 'james',
+  email: 'james@gmail.com',
+  password: 'password',
+  phone: '91234567',
+  address: 'Sentosa',
+  DOB: '2020-05-05',
+  answer: 'Badminton'
+};
+const mockNavigateFunction = jest.fn();
+
+//Helper function to render Register component
+const renderRegisterComponent = () => {
+  render(
+    <MemoryRouter initialEntries={['/register']}>
+      <Routes>
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </MemoryRouter>
+  );
+}
+
+//Helper function to fill in fields and press REGISTER button
+const fillInFieldsAndRegister = () => {
+  fireEvent.change(screen.getByPlaceholderText('Enter Your Name'), { target: { value: sampleInput.name } });
+  fireEvent.change(screen.getByPlaceholderText('Enter Your Email'), { target: { value: sampleInput.email } });
+  fireEvent.change(screen.getByPlaceholderText('Enter Your Password'), { target: { value: sampleInput.password } });
+  fireEvent.change(screen.getByPlaceholderText('Enter Your Phone'), { target: { value: sampleInput.phone } });
+  fireEvent.change(screen.getByPlaceholderText('Enter Your Address'), { target: { value: sampleInput.address } });
+  fireEvent.change(screen.getByPlaceholderText('Enter Your DOB'), { target: { value: sampleInput.DOB } });
+  fireEvent.change(screen.getByPlaceholderText('What is Your Favorite sports'), { target: { value: sampleInput.answer } });
+  fireEvent.click(screen.getByRole('button', { name: 'REGISTER' }));
+}
+
+
 // Mock modules
 jest.mock('axios');
-jest.mock('react-hot-toast');
+jest.mock('react-hot-toast', () => ({
+  success: jest.fn(),
+  error: jest.fn(),
+}));
 jest.mock('react-router-dom', () => ({
   ...jest.requireActual('react-router-dom'),
-  useNavigate: jest.fn(),
+  useNavigate: () => mockNavigateFunction,
 }));
-
 //Mock the external components that Register component uses
 jest.mock('./../../components/Layout', () => ({ children }) => <div><h1>Mocked Register - Ecommerce App</h1><div>{ children }</div></div>);
 
@@ -30,13 +68,7 @@ describe('Register Component', () => {
         //ARRANGE
   
         //ACTION
-        render(
-          <MemoryRouter initialEntries={['/register']}>
-            <Routes>
-              <Route path="/register" element={<Register />} />
-            </Routes>
-          </MemoryRouter>
-        );
+        renderRegisterComponent();
   
         //ASSERT
         expect(screen.getByText("Mocked Register - Ecommerce App")).toBeInTheDocument();
@@ -46,63 +78,46 @@ describe('Register Component', () => {
 
       describe('with the input fields', () => {
         //NEVER PASS
-        // it('where the placeholders of input fields should be correct', () => {
-        //   //ARRANGE
+        it('where the placeholders of input fields should be correct', () => {
+          //ARRANGE
   
-        //   //ACTION
-        //   render(
-        //     <MemoryRouter initialEntries={['/register']}>
-        //       <Routes>
-        //         <Route path="/register" element={<Register />} />
-        //       </Routes>
-        //     </MemoryRouter>
-        //   );
+          //ACTION
+          renderRegisterComponent();
   
-        //   //ASSERT
-        //   expect(screen.getByPlaceholderText('Enter Your Name')).toBeInTheDocument();
-        //   expect(screen.getByPlaceholderText('Enter Your Email')).toBeInTheDocument();
-        //   expect(screen.getByPlaceholderText('Enter Your Password')).toBeInTheDocument();
-        //   expect(screen.getByPlaceholderText('Enter Your Phone')).toBeInTheDocument();
-        //   expect(screen.getByPlaceholderText('Enter Your Address')).toBeInTheDocument();
-        //   expect(screen.getByPlaceholderText('Enter Your DOB')).toBeInTheDocument();
-        //   expect(screen.getByPlaceholderText('What Is Your Favorite Sport')).toBeInTheDocument();
-        // });
+          //ASSERT
+          expect(screen.getByPlaceholderText('Enter Your Name')).toBeInTheDocument();
+          expect(screen.getByPlaceholderText('Enter Your Email')).toBeInTheDocument();
+          expect(screen.getByPlaceholderText('Enter Your Password')).toBeInTheDocument();
+          expect(screen.getByPlaceholderText('Enter Your Phone')).toBeInTheDocument();
+          expect(screen.getByPlaceholderText('Enter Your Address')).toBeInTheDocument();
+          expect(screen.getByPlaceholderText('Enter Your DOB')).toBeInTheDocument();
+          expect(screen.getByPlaceholderText('What Is Your Favorite Sport')).toBeInTheDocument();
+        });
 
 
         //NEVER PASS
-        // it('where the type of input fields should be correct', () => {
-        //   //ARRANGE
+        it('where the type of input fields should be correct', () => {
+          //ARRANGE
   
-        //   //ACTION
-        //   render(
-        //     <MemoryRouter initialEntries={['/register']}>
-        //       <Routes>
-        //         <Route path="/register" element={<Register />} />
-        //       </Routes>
-        //     </MemoryRouter>
-        //   );
+          //ACTION
+          renderRegisterComponent();
   
-        //   //ASSERT
-        //   expect(screen.getByPlaceholderText('Enter Your Name')).toHaveAttribute('type', 'text');
-        //   expect(screen.getByPlaceholderText('Enter Your Email')).toHaveAttribute('type', 'email');
-        //   expect(screen.getByPlaceholderText('Enter Your Password')).toHaveAttribute('type', 'password');
-        //   expect(screen.getByPlaceholderText('Enter Your Phone')).toHaveAttribute('type', 'text');
-        //   expect(screen.getByPlaceholderText('Enter Your Address')).toHaveAttribute('type', 'text');
-        //   expect(screen.getByPlaceholderText('Enter Your DOB')).toHaveAttribute('type', 'date');
-        //   expect(screen.getByPlaceholderText('What Is Your Favorite Sport')).toHaveAttribute('type', 'text');
-        // });
+          //ASSERT
+          expect(screen.getByPlaceholderText('Enter Your Name')).toHaveAttribute('type', 'text');
+          expect(screen.getByPlaceholderText('Enter Your Email')).toHaveAttribute('type', 'email');
+          expect(screen.getByPlaceholderText('Enter Your Password')).toHaveAttribute('type', 'password');
+          expect(screen.getByPlaceholderText('Enter Your Phone')).toHaveAttribute('type', 'text');
+          expect(screen.getByPlaceholderText('Enter Your Address')).toHaveAttribute('type', 'text');
+          expect(screen.getByPlaceholderText('Enter Your DOB')).toHaveAttribute('type', 'date');
+          expect(screen.getByPlaceholderText('What Is Your Favorite Sport')).toHaveAttribute('type', 'text');
+        });
+
 
         it('where the input fields should all initially be empty', () => {
           //ARRANGE
   
           //ACTION
-          render(
-            <MemoryRouter initialEntries={['/register']}>
-              <Routes>
-                <Route path="/register" element={<Register />} />
-              </Routes>
-            </MemoryRouter>
-          );
+          renderRegisterComponent();
   
           //ASSERT
           expect(screen.getByPlaceholderText('Enter Your Name').value).toBe('');
@@ -127,25 +142,19 @@ describe('Register Component', () => {
             password: 'password',
             phone: '91234567',
             address: 'Sentosa',
-            dob: '2020-05-05',
-            sport: 'Badminton'
+            DOB: '2020-05-05',
+            answer: 'Badminton'
           };
 
           //ACTION
-          render(
-            <MemoryRouter initialEntries={['/register']}>
-              <Routes>
-                <Route path="/register" element={<Register />} />
-              </Routes>
-            </MemoryRouter>
-          );
+          renderRegisterComponent();
           fireEvent.change(screen.getByPlaceholderText('Enter Your Name'), { target: { value: sampleInput.name } });
           fireEvent.change(screen.getByPlaceholderText('Enter Your Email'), { target: { value: sampleInput.email } });
           fireEvent.change(screen.getByPlaceholderText('Enter Your Password'), { target: { value: sampleInput.password } });
           fireEvent.change(screen.getByPlaceholderText('Enter Your Phone'), { target: { value: sampleInput.phone } });
           fireEvent.change(screen.getByPlaceholderText('Enter Your Address'), { target: { value: sampleInput.address } });
-          fireEvent.change(screen.getByPlaceholderText('Enter Your DOB'), { target: { value: sampleInput.dob } });
-          fireEvent.change(screen.getByPlaceholderText('What is Your Favorite sports'), { target: { value: sampleInput.sport } });
+          fireEvent.change(screen.getByPlaceholderText('Enter Your DOB'), { target: { value: sampleInput.DOB } });
+          fireEvent.change(screen.getByPlaceholderText('What is Your Favorite sports'), { target: { value: sampleInput.answer } });
 
           //ASSERT
           expect(screen.getByPlaceholderText('Enter Your Name').value).toBe(sampleInput.name);
@@ -153,12 +162,12 @@ describe('Register Component', () => {
           expect(screen.getByPlaceholderText('Enter Your Password').value).toBe(sampleInput.password);
           expect(screen.getByPlaceholderText('Enter Your Phone').value).toBe(sampleInput.phone);
           expect(screen.getByPlaceholderText('Enter Your Address').value).toBe(sampleInput.address);
-          expect(screen.getByPlaceholderText('Enter Your DOB').value).toBe(sampleInput.dob);
+          expect(screen.getByPlaceholderText('Enter Your DOB').value).toBe(sampleInput.DOB);
           /* Previous test already caught error with the placeholder text for the sports field
              Hence, I still use the wrong placeholder because intention of this test is to check whether the
              input fields allow typing and not label
           */
-          expect(screen.getByPlaceholderText('What is Your Favorite sports').value).toBe(sampleInput.sport);
+          expect(screen.getByPlaceholderText('What is Your Favorite sports').value).toBe(sampleInput.answer);
         });
       });
 
@@ -167,70 +176,148 @@ describe('Register Component', () => {
         //ARRANGE
   
         //ACTION
-        render(
-          <MemoryRouter initialEntries={['/register']}>
-            <Routes>
-              <Route path="/register" element={<Register />} />
-            </Routes>
-          </MemoryRouter>
-        );
+        renderRegisterComponent();
   
         //ASSERT
         expect(screen.getByRole('button', { name: 'REGISTER' })).toBeInTheDocument();
       });
+  });
 
 
+  describe ('should correctly offer the register functionality', () => {
+    let consoleLogSpy;
 
+    beforeEach(() => {
+      //Reinitialise console log spy
+      consoleLogSpy = jest.spyOn(console, 'log').mockImplementation(() => {});
     });
-  
+    
+    afterEach(() => {
+      //Restore original functionality of console.log
+      consoleLogSpy?.mockRestore();
+    });
 
-  // it('should register the user successfully', async () => {
-  //   axios.post.mockResolvedValueOnce({ data: { success: true } });
 
-  //   const { getByText, getByPlaceholderText } = render(
-  //       <MemoryRouter initialEntries={['/register']}>
-  //         <Routes>
-  //           <Route path="/register" element={<Register />} />
-  //         </Routes>
-  //       </MemoryRouter>
-  //     );
+    it('where it provides a success message and navigates to /login upon successful registration', async () => {
+      //ARRANGE
+      axios.post.mockResolvedValueOnce({
+        data: { success: true, message: 'Successful registration' },
+      });
 
-  //   fireEvent.change(getByPlaceholderText('Enter Your Name'), { target: { value: 'John Doe' } });
-  //   fireEvent.change(getByPlaceholderText('Enter Your Email'), { target: { value: 'test@example.com' } });
-  //   fireEvent.change(getByPlaceholderText('Enter Your Password'), { target: { value: 'password123' } });
-  //   fireEvent.change(getByPlaceholderText('Enter Your Phone'), { target: { value: '1234567890' } });
-  //   fireEvent.change(getByPlaceholderText('Enter Your Address'), { target: { value: '123 Street' } });
-  //   fireEvent.change(getByPlaceholderText('Enter Your DOB'), { target: { value: '2000-01-01' } });
-  //   fireEvent.change(getByPlaceholderText('What is Your Favorite sports'), { target: { value: 'Football' } });
+      //ACTION
+      renderRegisterComponent();
+      fillInFieldsAndRegister();
 
-  //   fireEvent.click(getByText('REGISTER'));
+      //ASSERT
+      await waitFor(() =>  {
+        expect(axios.post).toHaveBeenCalledTimes(1)
+        expect(axios.post).toHaveBeenCalledWith("/api/v1/auth/register", sampleInput);
+        expect(toast.success).toHaveBeenCalledTimes(1);
+        expect(toast.success).toHaveBeenCalledWith('Register Successfully, please login');
+        expect(mockNavigateFunction).toHaveBeenCalledTimes(1);
+        expect(mockNavigateFunction).toHaveBeenCalledWith('/login');
+      });
+    });
 
-  //   await waitFor(() => expect(axios.post).toHaveBeenCalled());
-  //   expect(toast.success).toHaveBeenCalledWith('Register Successfully, please login');
-  // });
 
-  // it('should display error message on failed registration', async () => {
-  //   axios.post.mockRejectedValueOnce({ message: 'User already exists' });
+    it ('where it displays an error message if register POST request did not return any value for the success attribute', async () => {
+      //ARRANGE
+      axios.post.mockResolvedValueOnce({
+        data: { message: 'Name is required' },
+      });
 
-  //   const { getByText, getByPlaceholderText } = render(
-  //       <MemoryRouter initialEntries={['/register']}>
-  //         <Routes>
-  //           <Route path="/register" element={<Register />} />
-  //         </Routes>
-  //       </MemoryRouter>
-  //     );
+      //ACTION
+      renderRegisterComponent();
+      fillInFieldsAndRegister();
 
-  //   fireEvent.change(getByPlaceholderText('Enter Your Name'), { target: { value: 'John Doe' } });
-  //   fireEvent.change(getByPlaceholderText('Enter Your Email'), { target: { value: 'test@example.com' } });
-  //   fireEvent.change(getByPlaceholderText('Enter Your Password'), { target: { value: 'password123' } });
-  //   fireEvent.change(getByPlaceholderText('Enter Your Phone'), { target: { value: '1234567890' } });
-  //   fireEvent.change(getByPlaceholderText('Enter Your Address'), { target: { value: '123 Street' } });
-  //   fireEvent.change(getByPlaceholderText('Enter Your DOB'), { target: { value: '2000-01-01' } });
-  //   fireEvent.change(getByPlaceholderText('What is Your Favorite sports'), { target: { value: 'Football' } });
+      //ASSERT
+      await waitFor(() =>  {
+        expect(axios.post).toHaveBeenCalledTimes(1)
+        expect(axios.post).toHaveBeenCalledWith("/api/v1/auth/register", sampleInput);
+        expect(toast.error).toHaveBeenCalledTimes(1);
+        expect(toast.error).toHaveBeenCalledWith('Name is required');
+      });
+    });
 
-  //   fireEvent.click(getByText('REGISTER'));
 
-  //   await waitFor(() => expect(axios.post).toHaveBeenCalled());
-  //   expect(toast.error).toHaveBeenCalledWith('Something went wrong');
-  // });
+    it ('where it displays an error message if register POST request indicates false for success attribute', async () => {
+      //ARRANGE
+      axios.post.mockResolvedValueOnce({
+        data: { success: false, message: 'Already registered, please login' },
+      });
+
+      //ACTION
+      renderRegisterComponent();
+      fillInFieldsAndRegister();
+
+      //ASSERT
+      await waitFor(() =>  {
+        expect(axios.post).toHaveBeenCalledTimes(1)
+        expect(axios.post).toHaveBeenCalledWith("/api/v1/auth/register", sampleInput);
+        expect(toast.error).toHaveBeenCalledTimes(1);
+        expect(toast.error).toHaveBeenCalledWith('Already registered, please login');
+      });
+    });
+
+
+    it ('where it displays an error message if an exception occurs during the registration process', async () => {
+      //ARRANGE
+      const error = new Error('Exception during registration')
+      axios.post.mockRejectedValueOnce(error);
+
+      //ACTION
+      renderRegisterComponent();
+      fillInFieldsAndRegister();
+
+      //ASSERT
+      await waitFor(() =>  {
+        expect(axios.post).toHaveBeenCalledTimes(1)
+        expect(axios.post).toHaveBeenCalledWith("/api/v1/auth/register", sampleInput);
+        expect(consoleLogSpy).toHaveBeenCalledWith(error);
+        expect(toast.error).toHaveBeenCalledTimes(1);
+        expect(toast.error).toHaveBeenCalledWith('Something went wrong');
+      });
+    });
+
+
+    it ('where it displays an error message if axios.post returns a null value', async () => {
+      //ARRANGE
+      axios.post.mockResolvedValueOnce(null);
+
+      //ACTION
+      renderRegisterComponent();
+      fillInFieldsAndRegister();
+
+      //ASSERT
+      await waitFor(() =>  {
+        expect(axios.post).toHaveBeenCalledTimes(1)
+        expect(axios.post).toHaveBeenCalledWith("/api/v1/auth/register", sampleInput);
+        expect(toast.error).toHaveBeenCalledTimes(1);
+        //When res is null, trying to read res.data.message leads to an exception
+        expect(toast.error).toHaveBeenCalledWith("Something went wrong");
+        expect(consoleLogSpy).toHaveBeenCalledWith(new Error("Cannot read properties of null (reading 'data')"));
+
+      });
+    });
+
+
+    it ('where it displays an error message if axios.post returns a response without data attribute', async () => {
+      //ARRANGE
+      axios.post.mockResolvedValueOnce({});
+
+      //ACTION
+      renderRegisterComponent();
+      fillInFieldsAndRegister();
+
+      //ASSERT
+      await waitFor(() =>  {
+        expect(axios.post).toHaveBeenCalledTimes(1)
+        expect(axios.post).toHaveBeenCalledWith("/api/v1/auth/register", sampleInput);
+        expect(toast.error).toHaveBeenCalledTimes(1);
+        //Trying to read success attribute of undefined leads to exception, since res.data is undefined
+        expect(toast.error).toHaveBeenCalledWith("Something went wrong");
+        expect(consoleLogSpy).toHaveBeenCalledWith(new Error("Cannot read properties of undefined (reading 'success')"));
+      });
+    });
+  });
 });
