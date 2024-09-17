aaa8f58ef918dad9b812875dfee33eb3a2107d23
kohkaixun <kkx1929@gmail.com>
2024-09-17 13:12:36 +0800

    Add comment to last test Unsure if test 5, 6 and 7 should be added as they seem to be ensuring that the code accepts a null value as a valid response. However, the axios.get function seems to either return the response or throw an error. In this case, a null response would indicate that the error is in the API implementation and not the call. As such, should these tests be added?

--- client/src/components/Routes/Private.test.js
+++ client/src/components/Routes/Private.test.js
@@ -95,6 +95,12 @@ describe('PrivateRoute', () => {
         expect(await screen.findByText('Mocked Spinner')).toBeInTheDocument();
     });
 
+    /**
+     * Unsure if this test should be added as this seems to be ensuring that the code accepts 
+     * a null value as a valid response. The axios.get function seems to either return the 
+     * response or throw an error. In this case, a null response would indicate that the error 
+     * is in the API implementation and not the call.
+     */
     it("should render Spinner if axios responses's data field's ok field is null", async () => {
         useAuth.mockReturnValue([mockAuth, jest.fn()]);
         const res = { data: { ok: null } };
