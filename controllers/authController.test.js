8d995dceb21968b4107a0a4cf6fc7001c355f95c
kohkaixun <kkx1929@gmail.com>
2024-09-30 00:47:32 +0800

    Add unit test for first populate function error

--- controllers/authController.test.js
+++ controllers/authController.test.js
@@ -410,6 +410,21 @@ describe("getOrdersController", () => {
         expect(res.send).toHaveBeenCalledTimes(1);
         expect(res.send).toHaveBeenCalledWith(expect.objectContaining({ success: false, error: mockedFindError }));
     });
+
+    it("should catch error thrown by frst populate function", async () => {
+        const mockedPopulateError = new Error("find failed");
+        orderModel.find.mockImplementation(() => ({ populate: jest.fn(() => {
+            throw mockedPopulateError;
+        }) }));
+        await getOrdersController(req, res);
+        expect(orderModel.find).toHaveBeenCalledTimes(1);
+        expect(orderModel.find).toHaveBeenCalledWith({ buyer: req.user._id });
+        expect(res.json).toHaveBeenCalledTimes(0);
+        expect(res.status).toHaveBeenCalledTimes(1);
+        expect(res.status).toHaveBeenCalledWith(500);
+        expect(res.send).toHaveBeenCalledTimes(1);
+        expect(res.send).toHaveBeenCalledWith(expect.objectContaining({ success: false, error: mockedPopulateError }));
+    });
 });
 
 // Tests for orderStatusController
