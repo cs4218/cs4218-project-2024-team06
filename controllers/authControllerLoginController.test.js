cba976879867a8358ec094979a153c02fdbec5a6
eugenetangkj <dragoneugene10@gmail.com>
2024-10-03 16:21:24 +0800

    Tidy login controller

--- controllers/authControllerLoginController.test.js
+++ controllers/authControllerLoginController.test.js
@@ -179,25 +179,6 @@ describe('loginController', () => {
             token: "token"
             });
         });
-
-
-        // it('where it denies log in if there is an error', async () => {
-        //     //ARRANGE
-        //     const error = new Error('Exception during login');
-        //     JWT.sign.mockImplementation(() => {
-        //         throw error;
-        //     });
-
-        //     //ACTION
-        //     await loginController(req, res);
-
-        //     //ASSERT
-        //     expect(res.status).toHaveBeenCalledTimes(1);
-        //     expect(res.status).toHaveBeenCalledWith(500);
-        //     expect(res.send).toHaveBeenCalledTimes(1);
-        //     expect(res.send).toHaveBeenCalledWith({success: false, message: "Error in login", error});
-        //     expect(consoleLogSpy).toHaveBeenCalledWith(error);
-        // });
     });
 
 
