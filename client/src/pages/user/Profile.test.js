75236e1943899dc85c349c33055476edcb45b271
kohkaixun <kkx1929@gmail.com>
2024-10-05 12:55:45 +0800

    Add initial render unit test for Profile page

---
+++ client/src/pages/user/Profile.test.js
@@ -0,0 +1,61 @@
+import '@testing-library/jest-dom';
+import axios from "axios";
+import Profile from "./Profile";
+import { render, screen } from '@testing-library/react';
+import toast from "react-hot-toast";
+import { useAuth } from "../../context/auth";
+
+jest.mock('../../context/auth', () => ({
+    useAuth: jest.fn()
+}));
+
+jest.mock("axios", () => ({
+    put: jest.fn(),
+}));
+
+jest.mock("react-hot-toast", () => ({
+    error: jest.fn(),
+    success: jest.fn(),
+}));
+
+jest.mock('../../components/Layout', () => ({ children }) => <div>{children}</div>);
+jest.mock('../../components/UserMenu', () => () => <div>User Menu</div>);
+
+const mockUser = {
+    user: {
+        name: "Halimah Yacob",
+        email: "halimah@gov.sg", 
+        password: "safePassword", 
+        phone: "999", 
+        address: "Istana"
+    }, 
+    token: "mockedToken"
+};
+
+const emptyString = "";
+
+Object.defineProperty(window, "localStorage", {
+    value: {
+        getItem: jest.fn(() => JSON.stringify(mockUser)),
+        setItem: jest.fn(),
+    },
+    writable: true,
+});
+
+describe("Profile", () => {
+    beforeEach(() => {
+        jest.clearAllMocks();
+        useAuth.mockReturnValue([mockUser, jest.fn()]);
+    });
+
+    it("should initially render the user's information except for password", () => {
+        const { name, email, password, phone, address } = mockUser.user;
+        render(<Profile />);
+        expect(screen.getByDisplayValue(name)).toBeInTheDocument();
+        expect(screen.getByDisplayValue(email)).toBeInTheDocument();
+        expect(screen.getByDisplayValue(emptyString)).toBeInTheDocument(); // Password is not displayed
+        expect(screen.queryByDisplayValue(password)).not.toBeInTheDocument(); // Password is not displayed
+        expect(screen.getByDisplayValue(phone)).toBeInTheDocument();
+        expect(screen.getByDisplayValue(address)).toBeInTheDocument();
+    });
+});
