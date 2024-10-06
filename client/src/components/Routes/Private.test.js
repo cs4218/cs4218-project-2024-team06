859f2fbadafcbccdda763387fb7b6f86666b2101
kohkaixun <kkx1929@gmail.com>
2024-10-07 05:27:54 +0800

    Update unit test for Private

--- client/src/components/Routes/Private.test.js
+++ client/src/components/Routes/Private.test.js
@@ -11,13 +11,6 @@ jest.mock('../../context/auth', () => ({
     useAuth: jest.fn()
 }));
 
-// jest.mock('mongoose', () => ({
-//     connect: jest.fn(),
-//     model: jest.fn(),
-//     Schema: jest.fn(),
-// }));
-
-// Sample token for testing
 const mockAuth = { token: 'sample token' };
 
 jest.mock('axios');
@@ -61,56 +54,4 @@ describe('PrivateRoute', () => {
         expect(await screen.findByText('Mocked Spinner')).toBeInTheDocument();
         expect(axios.get).toHaveBeenCalledWith(apiString);
     });
-
-    // This test is skipped because it is not working as expected
-    it.skip('should render Spinner if axios request fails', async () => {
-        useAuth.mockReturnValue([mockAuth, jest.fn()]);
-        axios.get.mockRejectedValue(new Error('Get request failed'));
-        render(<PrivateRoute />);
-        expect(await screen.findByText('Mocked Spinner')).toBeInTheDocument();
-    });
-
-    // This test is skipped because it is not working as expected
-    /**
-     * Unsure if this test should be added as this seems to be ensuring that the code accepts 
-     * a null value as a valid response. The axios.get function seems to either return the 
-     * response or throw an error. In this case, a null response would indicate that the error 
-     * is in the API implementation and not the call.
-     */
-    it.skip("should render Spinner if axios response is null", async () => {
-        useAuth.mockReturnValue([mockAuth, jest.fn()]);
-        const res = null;
-        axios.get.mockResolvedValue(res);
-        render(<PrivateRoute />);
-        expect(await screen.findByText('Mocked Spinner')).toBeInTheDocument();
-    });
-
-    // This test is skipped because it is not working as expected
-    /**
-     * Unsure if this test should be added as this seems to be ensuring that the code accepts 
-     * a null value as a valid response. The axios.get function seems to either return the 
-     * response or throw an error. In this case, a null response would indicate that the error 
-     * is in the API implementation and not the call.
-     */
-    it.skip("should render Spinner if axios response's data field is null", async () => {
-        useAuth.mockReturnValue([mockAuth, jest.fn()]);
-        const res = { data: null };
-        axios.get.mockResolvedValue(res);
-        render(<PrivateRoute />);
-        expect(await screen.findByText('Mocked Spinner')).toBeInTheDocument();
-    });
-
-    /**
-     * Unsure if this test should be added as this seems to be ensuring that the code accepts 
-     * a null value as a valid response. The axios.get function seems to either return the 
-     * response or throw an error. In this case, a null response would indicate that the error 
-     * is in the API implementation and not the call.
-     */
-    it("should render Spinner if axios responses's data field's ok field is null", async () => {
-        useAuth.mockReturnValue([mockAuth, jest.fn()]);
-        const res = { data: { ok: null } };
-        axios.get.mockResolvedValue(res);
-        render(<PrivateRoute />);
-        expect(await screen.findByText('Mocked Spinner')).toBeInTheDocument();
-    })
 });
