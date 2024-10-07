b63664abd6c207da5bbbf4883ee3a316d38e3a18
Aishwarya-Hariharan-Iyer <e0893448@u.nus.edu>
2024-10-07 10:51:07 +0800

    Add test failing instead

--- controllers/categoryController.test.js
+++ controllers/categoryController.test.js
@@ -23,7 +23,7 @@ describe('category controller', () => {
         };
     });
 
-    test('should return 200 and get all categories', async () => {
+    test.failing('should return 200 and get all categories', async () => {
         categoryModel.find = jest.fn().mockResolvedValue({});
 
         await categoryController(req, res);
@@ -35,7 +35,7 @@ describe('category controller', () => {
         })
     });
 
-    test('should return 500 while getting categories', async () => {
+    test.failing('should return 500 while getting categories', async () => {
         consoleLogSpy = jest.spyOn(console, 'log').mockImplementation(() => {error});
         categoryModel.find = jest.fn().mockRejectedValue(error);
 
