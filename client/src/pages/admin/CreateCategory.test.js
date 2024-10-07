54e8b3fe6360ceecb9aa849f65b6ebfaf412cddc
Aishwarya-Hariharan-Iyer <e0893448@u.nus.edu>
2024-10-07 11:07:43 +0800

    Migrate client tests to new branch

---
+++ client/src/pages/admin/CreateCategory.test.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import axios from "axios";
+import { render, waitFor, screen, fireEvent } from '@testing-library/react';
+import { MemoryRouter, Routes, Route } from 'react-router-dom';
+import CreateCategory from './CreateCategory';
+import '@testing-library/jest-dom/extend-expect';
+
+//Mock components
+jest.mock('axios');
+jest.mock('../../components/AdminMenu', () => () => <div>Admin Menu Mock</div>);
+jest.mock('./../../components/Layout', () => ({ children }) => <div><h1>Dashboard Mock</h1><div>{ children }</div></div>);
+jest.mock('./../../components/Form/CategoryForm', () => ({ children }) => <div><h1>Category Form Mock</h1><div>{ children }</div></div>);
+
+
+describe("Create Category Component", () => {
+
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
+
+
+    describe("When the component is rendered", () => {
+        
+        test("The different components are rendered with successful fetching of categories", async () => {
+
+            //ARRANGE
+            // Mock data
+            const mockValidCategories = [
+                { _id: 1, name: 'dresses', slug: 'dresses' },
+                { _id: 2, name: 'shoes', slug: 'shoes' },
+                { _id: 3, name: 'books', slug: 'books' },
+                { _id: 4, name: 'food', slug: 'food' },
+                { _id: 5, name: 'toys', slug: 'toys' },
+                { _id: 6, name: 'pants', slug: 'pants' },
+            ];
+
+            //ACT
+            await axios.get.mockResolvedValueOnce({ data: {success: true, category: mockValidCategories }});
+        
+            render(
+                <MemoryRouter initialEntries={['/dashboard/admin/create-category']}>
+                    <Routes>
+                    <Route path='/dashboard/admin/create-category' element={<CreateCategory />} />
+                    </Routes>
+                </MemoryRouter>
+                );
+
+            //ASSERT
+            await waitFor(() => {
+                expect(screen.getByText(/Dashboard Mock/)).toBeInTheDocument(); 
+                expect(screen.getByText(/Admin Menu Mock/)).toBeInTheDocument();
+                expect(screen.getByText(/Category Form Mock/)).toBeInTheDocument();
+                expect(axios.get).toHaveBeenCalledWith("/api/v1/category/get-category");
+                expect(screen.getByText('dresses')).toBeInTheDocument();
+                expect(screen.getByText('shoes')).toBeInTheDocument();
+                expect(screen.getByText('toys')).toBeInTheDocument();
+                expect(screen.getByText('food')).toBeInTheDocument();
+                expect(screen.getByText('pants')).toBeInTheDocument();
+            });
+            
+        });
+    })
+})
