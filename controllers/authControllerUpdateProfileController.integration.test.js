d582a89a231663139d7646367d72af94a1f00102
kohkaixun <kkx1929@gmail.com>
2024-10-20 17:01:51 +0800

    Add integration test for updateProfileController

---
+++ controllers/authControllerUpdateProfileController.integration.test.js
@@ -0,0 +1,67 @@
+import mongoose from "mongoose";
+import { MongoMemoryServer } from 'mongodb-memory-server';
+
+const oldProfile = {
+    name: "Halimah Yacob", 
+    email: "yacob@gov.sg", 
+    password: "safePassword", 
+    address: "Not Istana", 
+    phone: "88888888"
+};
+
+const newProfile = {
+    name: "Tharman Shanmugaratnam", 
+    email: "tharman@gov.sg", 
+    password: "saferPassword", 
+    address: "Istana", 
+    phone: "999"
+}
+
+const newProfileUpdate = {
+    name: newProfile.name, 
+    password: newProfile.password, 
+    address: newProfile.address, 
+    phone: newProfile.phone
+}
+
+const req = {
+    body: {}, 
+    user: { _id: "123" }, 
+    params: { orderId: "mockedOrderId" }
+}
+
+const res = {
+    json: jest.fn(),
+    status: jest.fn(() => res),
+    send: jest.fn()
+};
+
+let mongoServer;
+
+describe("updateProfileController integration test", () => {
+    beforeAll(async () => {
+        mongoServer = await MongoMemoryServer.create();
+        const uri = mongoServer.getUri();
+        await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    });
+
+    const testCollectionName = "testUsers";
+
+    beforeEach(async () => {
+        await mongoose.connection.createCollection(testCollectionName);
+    });
+
+    afterEach(async () => {
+        await mongoose.connection.dropCollection(testCollectionName);
+    });
+
+    afterAll(async () => {
+        await mongoose.connection.dropDatabase();
+        await mongoose.disconnect();
+        await mongoServer.stop();
+    });
+
+    it("should integrate with userModel to update user profile", () => {
+        //
+    });
+});
