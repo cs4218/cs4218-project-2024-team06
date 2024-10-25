4110e6b6ecd6f8dad0001005f69cce076c985b37
kohkaixun <kkx1929@gmail.com>
2024-10-25 21:24:37 +0800

    Update Playwright config

--- global-playwright-setup.js
+++ global-playwright-setup.js
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 
 //Load environment variables from .env for Playwright tests
 dotenv.config({ path: './.env' });
+// process.env.MONGO_URL = process.env.MONGO_URL_TEST;
 
 export default async () => {
   console.log('Loaded environment variables for Playwright tests');
