197b4f3a86f6c5b60674d345d813aea2696c7851
kohkaixun <kkx1929@gmail.com>
2024-10-27 21:20:42 +0800

    Removing commented out code

--- global-playwright-setup.js
+++ global-playwright-setup.js
@@ -2,7 +2,6 @@ import dotenv from 'dotenv';
 
 //Load environment variables from .env for Playwright tests
 dotenv.config({ path: './.env' });
-// process.env.MONGO_URL = process.env.MONGO_URL_TEST;
 
 export default async () => {
   console.log('Loaded environment variables for Playwright tests');
